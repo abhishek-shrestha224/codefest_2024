c60b30dda0b9d08512bb44dd03be1f93ec4b2fa3
Abhishek Shrestha <meabhi224@gmail.com>
2024-12-13 20:33:55 +0545

    updated user service to realtions

--- server/src/auth/services.py
+++ server/src/auth/services.py
@@ -1,6 +1,8 @@
 from src.db.models import User, UserBase
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
+from sqlmodel.sql.expression import joinedload
+from datetime import datetime
 
 
 class UserService:
@@ -26,3 +28,30 @@ class UserService:
         await session.commit()
         await session.refresh(new_user)
         return new_user
+
+    async def update(self, user_id: str, user_data: UserBase, session: AsyncSession):
+        user_to_update = await self.retrieve(user_id, session)
+        if user_to_update is None:
+            return None
+        update_data = user_data.model_dump()
+        for k, v in update_data.items():
+            setattr(user_to_update, k, v)
+        user_to_update.updated_at = datetime.now()
+        await session.commit()
+        return user_to_update
+
+    async def delete(self, user_id: str, session: AsyncSession):
+        user_to_delete = await self.retrieve(user_id, session)
+        if user_to_delete is None:
+            return None
+        await session.delete(user_to_delete)
+        await session.commit()
+        return user_to_delete
+
+    async def retrieve_with_relations(self, user_id: str, session: AsyncSession):
+        statement = (
+            select(User).options(joinedload(User.location)).where(User.id == user_id)
+        )
+        resource = await session.exec(statement)
+        user = resource.first()
+        return user
